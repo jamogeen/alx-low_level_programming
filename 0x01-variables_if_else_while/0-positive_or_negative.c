6a30fe6922cd45865a784f58d43d125b30cf4782
jamogeen <jamogeen@gmail.com>
2022-06-06 16:47:12 -0700

    if conditions in C

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include <time.h>
 /* more headers goes there */
-
+#include <stdio.h>
 /* betty style doc for function main goes there */
 int main(void)
 {
@@ -12,13 +12,13 @@ int main(void)
 	/* your code goes there */
 	if (n == 0)
 	{
-		printf("%d is zero\n", &n);
+		printf("%d is zero\n", n);
 	} else if (n < 0)
 	{
-		printf("%d is negative\n", &n);
+		printf("%d is negative\n", n);
 	} else
 	{
-		printf("%d is positive\n", &n);
+		printf("%d is positive\n", n);
 	}
 	return (0);
 }
