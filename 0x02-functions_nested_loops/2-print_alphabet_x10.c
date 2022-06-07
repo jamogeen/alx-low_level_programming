de7c67bac88fa774fa3ca6e1fe0f0629d2385233
jamogeen <jamogeen@gmail.com>
2022-06-07 14:02:11 -0700

    nested loop

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -13,9 +13,10 @@ void print_alphabet_x10(void)
 
 	while (index <= 9)
 	{
-		for (letter = 'a'; letter <= 'z'; letter++)
+		for (letter = 'a'; letter <= 'z')
 		{
 			_putchar(letter);
+			letter++;
 		}
 		_putchar('\n');
 		index++;
