ffae803ac61e508f029ddc1b76c9a9c7cd38257f
jamogeen <jamogeen@gmail.com>
2022-06-07 13:20:29 -0700

    void print_alphabet(void) prototype

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - check the code
+ *
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		letter++;
+	}
+	_putchar('\n);
+}
