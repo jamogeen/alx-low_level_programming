a92042636b486b914577cc0634586757f4521923
jamogeen <jamogeen@gmail.com>
2022-06-07 16:53:52 -0700

    check if a character is an alphabet

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * isalpha - checks is a character in an alphabet
+ * @c: expects integer c
+ *
+ * Return: 1 if c is a letter,lowercase or uppercase 0 otherwise
+ */
+int isalpha(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
