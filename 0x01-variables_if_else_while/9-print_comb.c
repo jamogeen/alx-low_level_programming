a6a9a9f41ad6e7462373b85787d38c8e129d7e7d
jamogeen <jamogeen@gmail.com>
2022-06-06 19:47:51 -0700

    possible combination of single numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ *
+ * Return: success(0)
+ */
+int main(void)
+{
+	int i = 0;
+
+	for (; i <= 9; i++)
+	{
+		putchar(i + '0');
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
