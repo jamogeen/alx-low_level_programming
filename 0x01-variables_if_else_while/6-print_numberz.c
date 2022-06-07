79198b0ebc5a55ba7a1354828e0951037a338b88
jamogeen <jamogeen@gmail.com>
2022-06-06 19:38:40 -0700

    only using putchar to output int

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ *
+ * Return: success(0)
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
