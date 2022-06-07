c4e70623174372017b7ddc70dcfa835f6f4609e5
jamogeen <jamogeen@gmail.com>
2022-06-07 16:37:34 -0700

    function to check character case

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _islower - checks lower case and uppercase
+ * @c: expects integer c
+ *
+ * Return: 0 if lower case or 1 otherwise
+ */
+int _islower(int c);
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
+}
+
