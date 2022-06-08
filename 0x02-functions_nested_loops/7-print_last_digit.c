904aa4c68a7d00a6c42c3e9256d661536681a1ac
jamogeen <jamogeen@gmail.com>
2022-06-07 17:42:10 -0700

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+
+#include "main.h"
+
+/**
+ * print_last_digit - returns the last digit
+ * @i: expects integer i
+ *
+ * Return: returns the last digit of number
+ */
+int print_last_digit(int i)
+{
+	int last_digit;
+
+	last_digit = i % 10;
+	return (last_digit);
+}
