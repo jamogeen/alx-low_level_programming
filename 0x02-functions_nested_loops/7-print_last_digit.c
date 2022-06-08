6619b1f7ca82c8622cad8ec3ad2f09f440a2a7c9
jamogeen <jamogeen@gmail.com>
2022-06-07 18:04:18 -0700

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,7 +11,13 @@ int print_last_digit(int i)
 {
 	int last_digit;
 
+	if (i < 0)
+	{
+		i = i * -1;
+	}
+
 	last_digit = i % 10;
-	_putchar(last_digit);
+
+	_putchar(last_digit + '0');
 	return (last_digit);
 }
