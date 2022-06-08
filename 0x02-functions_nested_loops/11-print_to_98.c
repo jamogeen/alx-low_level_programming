fb57b570ead022a0cecbf15ad6518a23cdf8d18f
jamogeen <jamogeen@gmail.com>
2022-06-08 16:06:04 -0700

    range between n and 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,76 @@
+#include "main.h"
+
+/**
+ * print_to_98 - creates maths times table
+ *@n: expect n
+ */
+void print_to_98(int n)
+{
+	if (n == 98)
+	{
+		_putchar((n / 10) + '0');
+		_putchar((n % 10) + '0');
+	}
+	else if( n < 98)
+	{
+		for (n; n <= 98; n++)
+		{
+			if (n <= -1)
+			{
+				n = n * -1
+				if (n < 10)
+				{
+					_putchar('-');
+					_putchar(n + '0');
+				}
+				else
+				{
+					_putchar('-');
+					_putchar((n / 10) + '0');
+					_putchar((n % 10) + '0');
+					_putchar(',');
+				}
+			}
+			else if (n == 0)
+			{
+				_putchar(n + '0');
+				_putchar(',');
+			}
+			else if (n < 98 && n > 1)
+			{
+				_putchar((n / 10) + '0');
+				_putchar((n % 10) + '0');
+				_putchar(',');
+			}
+			else( n == 98)
+			{
+				_putchar((n / 10) + '0');
+				_putchar((n % 10) + '0');
+			}
+		}
+	}
+	else if (n > 98)
+	{
+		for (n; n >= 98; n--)
+		{
+			if (n > 99)
+			{
+				_putchar((n / 100) + '0');
+				_putchar((n / 10) + '0');
+				_putchar((n % 10) + '0');
+				_putchar(',');
+			}
+			else if(n == 99)
+			{
+				_putchar((n / 10) + '0');
+				_putchar((n % 10) + '0');
+				_putchar(',');
+			}
+			else if (n == 98)
+			{
+				_putchar((n / 10) + '0');
+				_putchar((n % 10) + '0');
+			}
+		}
+	}
+}
