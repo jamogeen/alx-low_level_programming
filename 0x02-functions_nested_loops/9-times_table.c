8982473d7843fb8a9252c2d65386ed5e45ea5033
jamogeen <jamogeen@gmail.com>
2022-06-08 13:01:31 -0700

    9 x 9 product table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,24 +1,39 @@
-
-#include "main.h"
+#include"main.h"
 
 /**
- * times_table - 9x9 mulyiplication table
+ * times_table - 9 x 9 product table
  *
  * Return: void
  */
 void times_table(void)
 {
-	int x, y;
+	int x, y, z;
 
-	for (x = 0; x <= 9; x++)
+	for (x = 0; x < 10; x++)
 	{
-		for (y = 0; y <= 9; y++)
+		for (y = 0; y < 10; y++)
 		{
-			_putchar((y * x) + '0');
-			_putchar(',');
-			_putchar(' ');
+			z = y * x;
+			if (y == 0)
+			{
+				_putchar(z + '0');
+			}
+			if (z < 10 && y!= 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(z + '0');
+			}
+			else if (z >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((z / 10) + '0');
+				_putchar((z % 10) + '0');
+
+			}
 		}
 		_putchar('\n');
 	}
 }
-
