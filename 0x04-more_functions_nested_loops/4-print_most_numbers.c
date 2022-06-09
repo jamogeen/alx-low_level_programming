d97c67878330f955478828c56c8e0c4578983d01
jamogeen <jamogeen@gmail.com>
2022-06-09 14:38:03 -0700

    print form 0 to 9 except 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,11 +8,14 @@ void print_most_numbers(void)
 {
 	int i;
 
-	for (i = 48; i <= 57; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		if (i != 50 || i != 52)
+		if (i != 2)
 		{
-			_putchar(i);
+			if (i != 4)
+			{
+				_putchar(i + '0');
+			}
 		}
 	}
 	_putchar('\n');
