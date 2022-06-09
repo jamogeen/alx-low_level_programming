e3632d0bde1f31f5a7af5dc39db5b2e9100c25cf
jamogeen <jamogeen@gmail.com>
2022-06-09 12:26:20 -0700

    product of two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include"main.h"
+
+/**
+ * mul - compute product of two int
+ *@a: first int
+ *@b: second int
+ *
+ * Return: return the product of a by b
+ */
+int mul(int a, int b)
+{
+	int product;
+
+	product = a 8 b;
+
+	return (product);
+
