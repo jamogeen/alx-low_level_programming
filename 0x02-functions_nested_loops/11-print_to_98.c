98984f10db992426b00d3f36ed26ed0de9cba607
jamogeen <jamogeen@gmail.com>
2022-06-08 18:01:12 -0700

    print n to range of 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,76 +1,34 @@
-#include "main.h"
+#include"main.h"
+#include<stdio.h>
 
 /**
  * print_to_98 - creates maths times table
+ *
  *@n: expect n
  */
 void print_to_98(int n)
 {
-	if (n == 98)
+	if (n <= 98)
 	{
-		_putchar((n / 10) + '0');
-		_putchar((n % 10) + '0');
-	}
-	else if(n < 98)
-	{
-		for (n <= 98; n++)
+		for (; n <= 98; n++)
 		{
-			if (n < 0)
+			printf("%d", n);
+			if (n != 98)
 			{
-				n = n * -1
-				if (n < 10)
-				{
-					_putchar('-');
-					_putchar(n + '0');
-				}
-				else
-				{
-					_putchar('-');
-					_putchar((n / 10) + '0');
-					_putchar((n % 10) + '0');
-					_putchar(',');
-				}
-			}
-			else if (n == 0)
-			{
-				_putchar(n + '0');
-				_putchar(',');
-			}
-			else if (n < 98 && n > 1)
-			{
-				_putchar((n / 10) + '0');
-				_putchar((n % 10) + '0');
-				_putchar(',');
-			}
-			else( n == 98)
-			{
-				_putchar((n / 10) + '0');
-				_putchar((n % 10) + '0');
+				printf(", ");
 			}
 		}
 	}
 	else if (n > 98)
 	{
-		for (n >= 98; n--)
+		for (; n >= 98; n--)
 		{
-			if (n > 99)
+			printf("%d", n);
+			if (n != 98)
 			{
-				_putchar((n / 100) + '0');
-				_putchar((n / 10) + '0');
-				_putchar((n % 10) + '0');
-				_putchar(',');
-			}
-			else if(n == 99)
-			{
-				_putchar((n / 10) + '0');
-				_putchar((n % 10) + '0');
-				_putchar(',');
-			}
-			else if (n == 98)
-			{
-				_putchar((n / 10) + '0');
-				_putchar((n % 10) + '0');
+				printf(", ");
 			}
 		}
 	}
+	printf("\n");
 }
