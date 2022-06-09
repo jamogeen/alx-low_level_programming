96e78202045f2ad9f5d9f7c4f20ef2f9b79682ea
jamogeen <jamogeen@gmail.com>
2022-06-09 11:05:54 -0700

    check if an alphabet is uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isupper - check if C is upper.
+ *@c: expect C
+ * Return: return 1 is C is upper or ) otherwise.
+ */
+int _isupper(int c)
+{
+	if (c <= 90 && c >= 65)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
