7079748d0c3771f3d9c535e3dcbc264dcef29072
jamogeen <jamogeen@gmail.com>
2022-06-09 16:49:27 -0700

    print square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_square - check the code
+ *@size: size of the sqaure
+ *
+ * Return: Always void
+ */
+int print_square(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else if (size >= 1 && size != 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 1; j <= size; j++)
+			{
+				_putchar('#');
+			}
+			if (j != size)
+			{
+				_putchar('\n');
+			}
+		}
+	}
+}
