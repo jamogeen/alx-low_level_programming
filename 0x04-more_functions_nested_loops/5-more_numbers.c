218023db910743bd1a94034c370757e90464018f
jamogeen <jamogeen@gmail.com>
2022-06-09 18:23:14 -0700

    number 1 to 14 , 10 times;

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,19 +7,20 @@
  */
 void more_numbers(void)
 {
-	int i, n;
+	int i;
+	int a = 0;
 
-	for (n = 0; n <= 10; n++)
+	while (a < 10)
 	{
 		for (i = 0; i <= 14; i++)
 		{
-
 			if (i >= 10)
 			{
-				_putchar((i / 10) + '0');
+				_putchar(i / 10 + '0');
 			}
-			_putchar((i % 10) + '0');
+			_putchar(i % 10 + '0');
 		}
+		a++;
 		_putchar('\n');
 	}
 }
