3e8744054d804800331af82a880a331a0bbb7c0d
jamogeen <jamogeen@gmail.com>
2022-06-13 11:37:16 -0700

    string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include<string.h>
+/**
+ * _strlen - compute the size of char
+ * @s: expect pointer s
+ *
+ * Return: return lenth of the string
+ */
+int _strlen(char *s)
+{
+	return (strlen(*s));
+}
