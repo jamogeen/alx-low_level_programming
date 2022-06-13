692eab31f19c2fefa182f50d3f3687fa204b8e69
jamogeen <jamogeen@gmail.com>
2022-06-13 15:45:52 -0700

    reverse sentense using pointer as ref

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev - print char in pointer in reverse
+ * @s: expect s
+ */
+void print_rev(char *s)
+{
+	int length = 0, i, x;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		length++;
+	}
+	for (x = 0; x <= length;)
+	{
+		_putchar(s[lenght]);
+		length--;
+	}
+	_putchar('\n');
+}
