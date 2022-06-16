d315a34e9fa6b66603fc5581f5959db51ac92d3d
jamogeen <jamogeen@gmail.com>
2022-06-16 16:45:11 -0700

    Concatenation of strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include"main.h"
+/**
+ * _strcat -  concatenates two strings
+ *@dest: expect dest
+ *@src: expect src char
+ *
+ * Return: return the concatenated string
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, x, length;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+		length++;
+	}
+
+	for (x = 0; src[x] != '\0'; length++)
+	{
+		dest[length] = src[x];
+		x++;
+	}
+	dest[length] = '\0';
+
+	return (dest);
+}
+
+
