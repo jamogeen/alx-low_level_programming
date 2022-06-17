73431420d06d4be9f867df1b3a33cf70ae6a6ddf
jamogeen <jamogeen@gmail.com>
2022-06-17 15:57:53 -0700

    strncpy function

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,5 +1,6 @@
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
+char *_strncpy(char *dest, char *src, int n);
 char *_strncat(char *dest, char *src, int n);
 char *leet(char *a);
 char *cap_string(char *a);
@@ -8,6 +9,5 @@ void reverse_array(int *a, int n);
 int _strcmp(char *s1, char *s2);
 char *rot13(char *a);
 void print_number(int n);
-char *_strncpy(char *dest, char *src, int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
